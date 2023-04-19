438567609c6940a3d10c5e8129c72ac6ace1342d
annary63 <125981972+annary63@users.noreply.github.com>
2023-04-19 11:38:56 +0300

    Create average_speed.py

---
+++ average_speed.py
@@ -0,0 +1,14 @@
+def average_speed(distance, time):
+    speed = distance / time
+    return speed
+
+distance1 = 50
+time1 = 2
+speed1 = average_speed(distance1, time1)
+
+distance2 = 100
+time2 = 3
+speed2 = average_speed(distance2, time2)
+
+print("Средняя скорость первого участка пути: ", speed1) # 25.0
+print("Средняя скорость второго участка пути: ", speed2) # 33.333333333333336
